09d262ae61b424ef11a4f6569d61e822942cdf3f
diegosouza007 <dsouza@yahoo.com>
2021-06-09 09:15:26 -0300

    code simplification

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@ let display = document.getElementById('display');
 
 let button = Array.from(document.getElementsByClassName('button'));
 
-let result = '';
-
 button.map(button => {
     button.addEventListener('click', (e) => {
         switch (e.target.innerText) {
             case '=':
-                result = display.innerHTML;
-                display.innerText = eval(result);
+                display.innerText = eval(display.innerText);
                 break;
 
             case 'C':
